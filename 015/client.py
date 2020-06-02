8b184361823af0d5d9be49f0661c91ec9cf9d616
Yeqing98 <1835132202@qq.com>
2020-06-02 10:52:08 +0800

    学习tcp套接字socket方法

---
+++ 015/client.py
@@ -0,0 +1,13 @@
+from socket import socket
+
+def main():
+    client = socket()
+
+    client.connect(('172.20.10.3', 6789))
+
+    print(client.recv(1024).decode('utf-8'))
+
+    client.close()
+
+if __name__ == '__main__':
+    main()
